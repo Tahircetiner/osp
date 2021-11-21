7bfcae2ab53f0ad8774aa1370c87ae35b287c1d2
tcetiner <tcetiner@neusta.de>
2021-11-21 21:16:33 +0100

    initial commit

---
+++ src/test/java/de/osp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.osp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
