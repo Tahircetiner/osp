5de00227ee454b30fd0c7616b6e9da6892e46e36
tcetiner <tcetiner@neusta.de>
2021-11-24 13:56:30 +0100

    changes

--- src/main/java/de/osp/Teacher.java
+++ src/main/java/de/osp/Teacher.java
@@ -9,12 +9,21 @@ import javax.persistence.Table;
 @Data
 @Entity
 public class Teacher {
-
     @Id
     private Long id;
 
     private String username;
 
     private String password;
+
+    public Teacher(){
+
+    }
+    public Teacher(Long id, String username, String password){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
 
 }
