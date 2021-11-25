7b1b49a66382af7c2cb967f2d50b800b2cd79b4e
Patrick <patrick.preis@gmail.com>
2021-11-25 14:56:49 +0100

    WordKlasse Implementier. Mann muss noch die Daten herkriegen die in Die generierung kommt.

--- src/main/java/de/osp/UserDatabaseApplication.java
+++ src/main/java/de/osp/UserDatabaseApplication.java
@@ -4,15 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import static de.osp.WordDateiService.erstelleAnmeldungAlsWordDokument;
-
 @EnableAsync
 @SpringBootApplication
 public class UserDatabaseApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(UserDatabaseApplication.class, args);
-        erstelleAnmeldungAlsWordDokument();
     }
-
 }
